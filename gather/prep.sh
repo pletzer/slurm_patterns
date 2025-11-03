ceec05f81fffb333fdcd7678dd18b3e9b926ffd1
Alex Pletzer <alexander@gokliya.net>
2025-11-03 16:16:19 +1300

    fix

--- gather/prep.sh
+++ gather/prep.sh
@@ -1,6 +1,6 @@
 # get the index from the argument
 idx="$1"
-sleep_time=((1 + RANDOM % 10))
+sleep_time=$((1 + RANDOM % 10))
 echo "Running prep $idx, sleeping $sleep_time s"
 sleep $sleep_time
 echo "Output from prep $idx"
